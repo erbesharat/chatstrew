6f1e7b46063e850e6acb2ec84700e830a2a2f712
Erfan Besharat <erbesharat@gmail.com>
2017-07-09 21:24:31 +0430

    Add migration for users role

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  require 'pry'
   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -41,7 +40,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    puts "program resumes here."
     devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :email, :password, :password_confirmation])
   end
 
