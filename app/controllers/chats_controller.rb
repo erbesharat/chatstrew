3e98a76aa062636004c5c9de5d8bf788a6c6e9d2
Erfan Besharat <erbesharat@gmail.com>
2017-07-10 13:06:06 +0430

    Add chats controller and add username for users

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,38 @@
+class ChatsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @chats = Chat.where(sender: current_user.username).or(Chat.where(receiver: current_user.username))
+  end
+  
+  def new
+    @chat = current_user.chats.build
+  end
+  
+  def create
+    @chat = current_user.chats.build(chat_params)    
+    if User.find_by(username: params[:chat][:receiver])
+      if @chat.save
+        redirect_to @chat
+      else
+        render 'new'
+      end
+    else
+      @chaterror = 'Can\'t find receiver'
+      render 'new'
+    end
+  end
+  
+  def show
+    @chat = Chat.find(params[:id])
+  end
+  
+  private
+
+  def chat_params
+    params.require(:chat).permit(:sender, :receiver)
+  end
+  
+  def find_chat
+    @chat = Chat.find(params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710064133) do
+ActiveRecord::Schema.define(version: 20170710074124) do
 
   create_table "chats", force: :cascade do |t|
     t.string "sender"
     t.string "receiver"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -41,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170710064133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "user"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
